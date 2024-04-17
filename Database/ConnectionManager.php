7ceccdf05c04005b5f5a4ff99af8146e8e844b69
LBmaster001 <lbmaster@gmx.de>
2024-04-17 23:54:53 +0200

    fixed config path

--- Database/ConnectionManager.php
+++ Database/ConnectionManager.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'config.php';
+include_once $_SERVER['DOCUMENT_ROOT']. '/config.php';
   function GetLocalMySQLConnection()
   {
 	 global $dBservername, $dBUsername, $dBPassword, $dBName;
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'config.php';
+include_once $_SERVER['DOCUMENT_ROOT']. '/config.php';
 $conn = GetDBConnection();
 
 if (!$conn) {
